2d81d787d7e739458b64a74bcb9a1c171d304cd2
mariam <mariamshahathamada@gmail.com>
2026-08-06 20:12:44 +0300

    n/a

--- utils/appError.js
+++ utils/appError.js
@@ -2,7 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = true; // نحدد أن الخطأ متوقع وناتج عن عملية عمل وليس خطأ غير متوقع في الكود
+    this.isOperational = true; 
 
     Error.captureStackTrace(this, this.constructor);
   }
